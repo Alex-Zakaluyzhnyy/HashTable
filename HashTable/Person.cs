68d19ca18f369399c4ab60790067a55d66336a33
Алексей Закалюжный <aleksey.zakaluzniy@gmail.com>
2019-11-06 21:29:16 +0300

    Добавьте файлы проекта.

---
+++ HashTable/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hastable = new SuperHashTable<Person>(100);
+            var persone = new Person() { Name = "Vasya", Age = 23, Gender = "Man"};
+            hastable.Add(new Person() { Name = "Aravan", Age = 13, Gender = "Man" });
+            hastable.Add(new Person() { Name = "Annush", Age = 25, Gender = "Woman" });
+            hastable.Add(persone);
+            Console.WriteLine(hastable.Search(new Person() { Name = "Annush", Age = 25, Gender = "Woman" }));
+            Console.WriteLine(hastable.Search(persone));
+            Console.ReadLine();
+        }
+    }
+}
